185bea0e5913335e60abd767b6bd5b7c9677296c
Cesar Ricardo <cesar.josep@hotmail.com>
2023-06-08 09:56:19 -0600

    Summary: Backend: Initial project setup Description: Set up the backend project structure and initial dependencies.

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import dotenv from "dotenv";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
